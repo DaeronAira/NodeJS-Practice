c7502dccc479626741096e46bd27c97dd5704b14
DaniilSan <lesskota52@gmail.com>
2023-10-10 19:27:08 +0300

    Работа с модулями

---
+++ 4-work-with-module/app.js
@@ -0,0 +1,13 @@
+/*var greeting1 = require('./greeting');
+console.log(`Hello ${greeting1.name}`);
+
+var greeting2 = require('./greeting');
+greeting2.name = 'Bob';
+
+console.log(`Hello ${greeting2.name}`);
+console.log(`Hello ${greeting1.name}`);*/
+
+const welcome = require('./welcome');
+
+welcome.getMorningMessage();
+welcome.getEveningMessage();
