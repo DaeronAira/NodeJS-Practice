3f4e4cdd1180684c56653e646151af29f4b052ec
DaniilSan <lesskota52@gmail.com>
2023-11-18 18:55:44 +0300

    Передача параметров приложению

---
+++ 6-passing-parameters/app.js
@@ -0,0 +1,12 @@
+let nodePath = process.argv[0];
+let appPath = process.argv[1];
+let name = process.argv[2];
+let surname = process.argv[3];
+let age = process.argv[4];
+
+console.log('nodePath: ' + nodePath);
+console.log('appPath: ' + appPath);
+console.log();
+console.log('name: ' + name);
+console.log('surname: ' + surname);
+console.log('age: ' + age);
